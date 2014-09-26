57bdc78150cd09bdb7a9a6de51cfb3365484ab34
Simeg44 <Simeg44@gmail.com>
2014-09-26 16:57:23 -0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,70 @@
+var players = [
+
+var ridiculus = {
+	username: "ridiculus",
+	fullname: "Acton Williamson",
+	region: "Europe",
+	race: "Zerg",
+	wins: 40,
+	losses: 234
+},
+
+var purus = {
+	username: "purus",
+	fullname: "Felix Bennett",
+	region: "Europe",
+	race: "Zerg",
+	wins: 152,
+	losses: 380
+},
+
+var interdum = {
+	username: "interdum",
+	fullname: "Stacy Ferrell",
+	region: "Korea",
+	race: "Zerg",
+	wins: 22,
+	losses: 136
+},
+
+var mauris = {
+	username: "mauris", 
+	fullname: "Olivia Higgins",
+	region: "Europe",
+	race: "Terran",
+	wins: 279,
+	losses: 374
+},
+
+var odio = {
+	username: "odio", 
+	fullname: "Addison Scott",
+	region: "Europe",
+	race: "Zerg",
+	wins: 62,
+	losses: 249
+},
+
+var scelerisque = {
+	username: "scelerisque", 
+	fullname: "Olivia Ruiz",
+	region: "America",
+	race: "Terran",
+	wins: 283,
+	losses: 24
+},
+
+var varius = {
+	username: "varius",
+	fullname: "Ulla Boyer",
+	region: "Korea",
+	race: "Protoss",
+	wins: 362,
+	losses: 437
+}];
+
+console.log(players);
+
+$(document).on('ready', function() {
+  
+});
